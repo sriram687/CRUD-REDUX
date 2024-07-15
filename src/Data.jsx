b2a730a1f54797642acbca59c96f472c3049b9a5
Kombiah Ramaiah <kombiahrk@yahoo.com>
2024-07-15 19:48:41 +0530

    crud operation project

---
+++ src/Data.jsx
@@ -0,0 +1,18 @@
+export const userList = [
+    {
+        name: "sriram",
+        email: "nq5p1@example.com",
+        id: 1
+    },
+    {
+        name: "srikanth",
+        email: "nq5p1@example.com",
+        id: 2
+    },
+    {
+        name: "ram",
+        email: "tdjwge@gmail.com",
+        id: 3
+
+    }
+]
